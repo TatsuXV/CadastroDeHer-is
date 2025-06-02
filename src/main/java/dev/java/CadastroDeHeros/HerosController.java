4a4174041a73b55a0f83d2725e96a184702c32bd
unknown <l.lilinaipe@hotmail.com>
2025-06-02 14:42:03 -0300

    Criado a controller

---
+++ src/main/java/dev/java/CadastroDeHeros/HerosController.java
@@ -0,0 +1,15 @@
+package dev.java.CadastroDeHeros;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class HerosController {
+
+    @GetMapping("bem-vindo")
+    public String boasVindas(){
+        return "Essa é a minha primeira menssagem";
+    }
+}
