3e0d4cc092056cf91883252116b9e345743b2226
unknown <l.lilinaipe@hotmail.com>
2025-06-02 12:26:59 -0300

    first commit

---
+++ src/main/java/dev/java/CadastroDeHeros/CadastroDeHerosApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDeHeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeHerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeHerosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/CadastroDeHeros/CadastroDeHerosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDeHeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeHerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
