519ad9b44656e64e892e3050d100c45f432e9f46
unknown <l.lilinaipe@hotmail.com>
2025-06-02 23:46:01 -0300

    cria uma entidade no banco de dados a partir do HeroModel

---
+++ src/main/java/dev/java/CadastroDeHeros/HeroModel.java
@@ -0,0 +1,47 @@
+package dev.java.CadastroDeHeros;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class HeroModel {
+
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public HeroModel(){}
+
+    public HeroModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
