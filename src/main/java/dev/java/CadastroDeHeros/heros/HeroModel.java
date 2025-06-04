22f5a04e835032a7d7ce55cbbf24395acea4f0d2
unknown <l.lilinaipe@hotmail.com>
2025-06-03 23:20:56 -0300

    feat: coloca o lombok na classe para abstração

--- src/main/java/dev/java/CadastroDeHeros/heros/HeroModel.java
+++ src/main/java/dev/java/CadastroDeHeros/heros/HeroModel.java
@@ -2,9 +2,16 @@ package dev.java.CadastroDeHeros.heros;
 
 import dev.java.CadastroDeHeros.trabalhos.MuralDeTrabalhos;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class HeroModel {
 
     @Id
@@ -17,36 +24,4 @@ public class HeroModel {
     @ManyToMany
     @JoinColumn(name = "trabalho_id")
     private MuralDeTrabalhos job;
-
-    public HeroModel(){}
-
-    public HeroModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
--- src/main/java/dev/java/CadastroDeHeros/trabalhos/MuralDeTrabalhos.java
+++ src/main/java/dev/java/CadastroDeHeros/trabalhos/MuralDeTrabalhos.java
@@ -2,11 +2,17 @@ package dev.java.CadastroDeHeros.trabalhos;
 
 import dev.java.CadastroDeHeros.heros.HeroModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_Jobs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MuralDeTrabalhos {
 
     @Id
@@ -14,7 +20,6 @@ public class MuralDeTrabalhos {
     long id;
 
     private String trabalho;
-
     private String dificuldade;
 
     @OneToMany(mappedBy = "trabalhos" )
