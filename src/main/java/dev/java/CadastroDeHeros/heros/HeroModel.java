af93aa9335ba40c557ec3593a72894c1330e628b
unknown <l.lilinaipe@hotmail.com>
2025-06-06 11:26:35 -0300

    fix: adiciona o @column na minha classe

--- src/main/java/dev/java/CadastroDeHeros/heros/HeroModel.java
+++ src/main/java/dev/java/CadastroDeHeros/heros/HeroModel.java
@@ -16,12 +16,16 @@ public class HeroModel {
 
     @Id
             @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
     private long id;
+
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "idade")
     private int idade;
 
     @ManyToMany
