f60f69efd4c4a5e4a63e8913ea4af2491f6d3835
unknown <l.lilinaipe@hotmail.com>
2025-07-07 16:19:33 -0300

    fix: inclui rank e img no model

--- src/main/java/dev/java/CadastroDeHeros/heros/HeroDTO.java
+++ src/main/java/dev/java/CadastroDeHeros/heros/HeroDTO.java
@@ -7,36 +7,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "tb_cadastro")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+public class NinjaDTO {
 
-public class HeroModel {
-
-    @Id
-            @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Id")
     private long id;
-
-    @Column(name = "nome")
     private String nome;
-
-    @Column(unique = true)
     private String email;
-
-    @Column(name = "idade")
     private int idade;
-
-    @Column(name = "rank")
     private String rank;
-
-    @Column(name = "imagem")
     private String iMG_url;
-
-    @ManyToOne
-    @JoinColumn(name = "trabalho_id")
-    @JsonIgnore
     private MuralDeTrabalhos job;
 }
